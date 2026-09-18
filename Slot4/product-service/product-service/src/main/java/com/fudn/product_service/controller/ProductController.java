8b09c8e3a0ea1eeccfc4602d183ae231491bf93a
doan <doanchauviet@gmail.com>
2026-09-18 15:35:52 +0700

    feat(controller): complete Put endpoint to update product base on Todo3

--- Slot4/product-service/product-service/src/main/java/com/fudn/product_service/controller/ProductController.java
+++ Slot4/product-service/product-service/src/main/java/com/fudn/product_service/controller/ProductController.java
@@ -1,12 +1,20 @@
 package com.fudn.product_service.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.fudn.product_service.dto.ProductRequest;
 import com.fudn.product_service.dto.ProductResponse;
 import com.fudn.product_service.service.ProductService;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
@@ -50,9 +58,11 @@ public class ProductController {
     //   public ProductResponse updateProduct(
     //          @PathVariable String id,
     //          @RequestBody ProductRequest productRequest) { ... }
-    // ==========================================================
-    // TODO: viết endpoint update tại đây
-
+    @PutMapping("/{id}")
+    public ProductResponse updateProduct(@PathVariable String id,
+                                        @RequestBody ProductRequest productRequest) {
+        return productService.updateProduct(id, productRequest);
+    }
 
     // ==========================================================
     // TODO 4 — Endpoint DELETE sản phẩm
