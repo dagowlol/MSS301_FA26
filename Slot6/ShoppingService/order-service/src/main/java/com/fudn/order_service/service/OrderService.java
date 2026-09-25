ffddf0c82a9a1413f92bee02d77583dd7544a8ba
doan <doanchauviet@gmail.com>
2026-09-25 16:56:20 +0700

    Add .gitignore, untrack IDE and MySQL runtime files

---
+++ Slot6/ShoppingService/order-service/src/main/java/com/fudn/order_service/service/OrderService.java
@@ -0,0 +1,43 @@
+package com.fudn.order_service.service;
+
+import com.fudn.order_service.client.InventoryClient;
+import com.fudn.order_service.dto.OrderRequest;
+import com.fudn.order_service.model.Order;
+import com.fudn.order_service.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient; // <-- thêm
+
+    public void placeOrder(OrderRequest orderRequest) {
+        boolean inStock = inventoryClient.isInStock(
+                orderRequest.skuCode(),
+                orderRequest.quantity());
+
+        if (inStock) {
+            var order = mapToOrder(orderRequest);
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException(
+                    "Product with Skucode " + orderRequest.skuCode() + " is not in stock");
+        }
+    }
+
+    private static Order mapToOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setPrice(orderRequest.price());
+        order.setQuantity(orderRequest.quantity());
+        order.setSkuCode(orderRequest.skuCode());
+        return order;
+    }
+}
